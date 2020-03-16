cbdc6282a3a5e3bcacf36377c4921a493410d58a
Radosław Kujawa <radoslaw.kujawa@c0ff33.net>
2020-03-16 01:32:43 +0100

    Iterate over issues, dump worklogs in CSV.

--- src/jih8t.py
+++ src/jih8t.py
@@ -8,26 +8,25 @@
 # issues_query: your JQL query
 #
 
-#from collections import Counter
 import yaml
 from jira import JIRA
 from pathlib import Path
 
+# WORKLOG_AUTHOR = "radoslaw.kujawa"
+
 with open(str(Path.home()) + "/.jih8t.yml", 'r') as ymlcfgfile:
     cfg = yaml.load(ymlcfgfile, yaml.FullLoader)
 
-jira = JIRA(server=cfg['jira']['server'], basic_auth=(cfg['jira']['user'], cfg['jira']['password'])) 
+jira = JIRA(server=cfg['jira']['server'], basic_auth=(cfg['jira']['user'], cfg['jira']['password']))
 
+# XXX search result can include worklog field if explicitedly asked for it
 issues = jira.search_issues(cfg['issues_query'])
 
-print(issues)
+for i in issues:
 
-# iterate over issues, query for issue individually to obtain worklog
-# store worklog entries from current time_period
+    issue_complete = jira.issue(str(i))
+    worklogs = issue_complete.fields.worklog.worklogs
 
-#print(issue.fields.worklog.worklogs[0].timeSpentSeconds)
+    for w in worklogs:
+        print(str(i) + "," + str(w.author) + "," + str(w.timeSpentSeconds))
 
-# Find the top three projects containing issues reported by admin
-#top_three = Counter([issue.fields.project.key for issue in issues]).most_common(3)
-
-#print(top_three)
